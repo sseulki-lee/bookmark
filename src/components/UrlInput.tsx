4f2c74bcb7cbc67fa5704d56b84013eff7e1d24a
sonjih00n <jihoon.son@worksmobile.com>
2021-06-02 21:28:51 +0900

    fix uri input

--- src/components/UrlInput.tsx
+++ src/components/UrlInput.tsx
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 
+const ENTER_KEY_CODE: string = "enter";
+const urlRegex: RegExp =
+  /^(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+
 type UrlInputProps = {
-  onSubmit: (url: string) => void;
+  handleButtonClick: (url: string) => void;
 };
-export default function UrlInput({ onSubmit }: UrlInputProps) {
+
+export default function UrlInput({ handleButtonClick }: UrlInputProps) {
   const [url, setUrl] = useState<string>("");
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setUrl(event.target.value);
   };
-  const handleSubmit = () => {
-    onSubmit(url);
+  const onClick = () => {
+    const urlValue = url;
+    urlRegex.test(urlValue)
+      ? handleButtonClick(urlValue)
+      : alert("올바르지 않은 형식입니다.");
+  };
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const { key } = event;
+    if (key.toLowerCase() === ENTER_KEY_CODE) {
+      onClick();
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="url" onChange={onChange} />
-      <input type="submit" value="추가" />
-    </form>
+    <div>
+      <input type="url" onChange={onChange} onKeyDown={onKeyDown} />
+      <button onClick={onClick}>추가</button>
+    </div>
   );
 }
